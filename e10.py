ff8b1eead784e137b212814562e294f2d861bd1f
komparse26 <komparse26@gmail.com>
2026-04-03 20:42:37 +0200

    Update e10.py

--- e10.py
+++ e10.py
@@ -68,7 +68,7 @@ html, body, [class*="css"]  {
 # ---------------------------------------------------------
 # 🧩 Header
 # ---------------------------------------------------------
-st.page_title("⛽ Benzinpreis‑Ticker")
+st.title("⛽ Benzinpreis‑Ticker")
 #st.subheader("Live‑Preise basierend auf deinem Standort")
 #st.info("Daten: Markttransparenzstelle (MTS-K) + OpenCage Geocoding")
 
